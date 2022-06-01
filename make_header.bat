7d13a47fbdc52ca3ba91b6ec882ae6c2a242a46f
mqhirr <myfionyt@gmail.com>
2022-06-01 11:45:03 +0300

    auto header maker

---
+++ make_header.bat
@@ -0,0 +1,29 @@
+mkdir include
+mkdir include\config
+mkdir include\events
+mkdir include\logger
+mkdir include\math
+mkdir include\player
+mkdir include\runner
+mkdir include\sdk
+mkdir include\server
+
+copy .\src\config\config.h include\config\
+copy .\src\config\macros.h include\config\
+
+copy .\src\events\manager.h include\events\
+copy .\src\events\context.h include\events\
+
+copy .\src\logger\logger.h include\logger\
+
+copy .\src\math\math.h include\math\
+
+copy .\src\player\player.h include\player\
+
+copy .\src\runner\run.h include\runner\
+
+copy .\src\sdk\packet.h include\sdk\
+copy .\src\sdk\update.h include\sdk\
+copy .\src\sdk\variant.h include\sdk\
+
+copy .\src\server\server.h include\server\
---
+++ make_header.sh
@@ -0,0 +1,20 @@
+mkdir include
+cp src/config/config.h include/config/
+cp src/config/macros.h include/config/
+
+cp src/events/manager.h include/events/
+cp src/events/context.h include/events/
+
+cp src/logger/logger.h include/logger/
+
+cp src/math/math.h include/math/
+
+cp src/player/player.h include/player/
+
+cp src/runner/run.h include/runner/
+
+cp src/sdk/packet.h include/sdk/
+cp src/sdk/update.h include/sdk/
+cp src/sdk/variant.h include/sdk/
+
+cp src/server/server.h include/server/
